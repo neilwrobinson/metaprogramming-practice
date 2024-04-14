bb90901d3488cafa6e34c613691ebf8a4aa6975a
Neil <neilwrobinson@users.noreply.github.com>
2024-04-14 13:45:25 -0400

    add eval test fixed

--- 05-Code_that_writes_itself/eval_test.rb
+++ 05-Code_that_writes_itself/eval_test.rb
@@ -31,8 +31,7 @@ class TestCheckedAttribute < Test::Unit::TestCase
 
     def test_accepts_valid_values
         pp @bob.public_methods.grep(/^a/)
-        #pp Kernel.public_methods.grep(/^a/)
-    ##    @bob.age = 20
-    #    assert_equal 20, @bob.age
+        @bob.age = 20
+        assert_equal 20, @bob.age
     end
 end
